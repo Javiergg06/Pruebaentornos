31a15ad407e9118ca4d7f1b35d94cb05afc14b71
Javiergg06 <javiergilgalan@gmail.com>
2025-11-11 16:18:38 +0100

    cambio

--- holamundo1.java
+++ holamundo1.java
@@ -1,16 +1,15 @@
 public class holamundo1 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         System.out.println("Hola Mundo");
-=======
+
         System.out.println("Hola Mundo22");
->>>>>>> Rama-nueva
-=======
+
+
         System.out.println("Hola Mundo");
         System.out.println("mi nombre es Javier y esto es una prueba de entornos")
->>>>>>> Rama-nueva
+
     }
     
 
